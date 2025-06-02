1b3143fa2a72d0bcfc1aff9d3065c78ec6ef3ed0
Tony <tonyguarino1489@gmail.com>
2025-06-02 18:00:17 -0400

    added framer motion dumbell

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import '../css/NavigationBar.css';
+import { motion } from 'framer-motion';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 
 export const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  return (
+    <Navbar expand='lg' className='position-absolute w-100 z-2'>
+      <Container>
+        <motion.a
+          className='navbar-brand text-light'
+          href='./'
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <FontAwesomeIcon
+            className='me-2'
+            icon={faDumbbell}
+            size='xl'
+            style={{ color: '#b197fc' }}
+          />
+        </motion.a>
+      </Container>
+    </Navbar>
+  );
 };
