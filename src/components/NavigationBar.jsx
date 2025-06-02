cf88b786ae1c8c8203d2b123e43d7772e7320c8b
Tony <tonyguarino1489@gmail.com>
2025-06-02 18:13:37 -0400

    finished remaining links inside motion.ul

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -24,7 +24,36 @@ export const NavigationBar = () => {
             size='xl'
             style={{ color: '#b197fc' }}
           />
+          Power <span className='text-uppercase fw-bold text-purple'>gym</span>
         </motion.a>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse
+          id='basic-navbar nav'
+          className='p-3 -p-lg-0 mt-2 mt-lg-0'
+        >
+          <motion.ul
+            className='navbar-nav me-auto justify-content-end w-100'
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <Nav.Link href='#' className='text-light text-uppercase'>
+              Home
+            </Nav.Link>
+            <Nav.Link href='#about' className='text-light text-uppercase'>
+              About
+            </Nav.Link>
+            <Nav.Link href='#features' className='text-light text-uppercase'>
+              Features
+            </Nav.Link>
+            <Nav.Link href='#plans' className='text-light text-uppercase'>
+              Plans
+            </Nav.Link>
+            <Nav.Link href='#contact' className='text-light text-uppercase'>
+              Contact
+            </Nav.Link>
+          </motion.ul>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
